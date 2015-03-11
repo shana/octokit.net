4e6e10b90f774c9ba9537e16d85275081cf6f863
Andreia Gaita <shana@github.com>
2015-03-10 17:46:09 -0700

    Strong name all the things

--- Octokit/Properties/AssemblyInfo.cs
+++ Octokit/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Octokit")]
 [assembly: AssemblyDescription("An async-based GitHub API client library for .NET")]
-[assembly: InternalsVisibleTo("Octokit.Tests")]
-[assembly: InternalsVisibleTo("Octokit.Tests-NetCore45")]
-[assembly: InternalsVisibleTo("Octokit.Tests-Portable")]
+//[assembly: InternalsVisibleTo("Octokit.Tests")]
+//[assembly: InternalsVisibleTo("Octokit.Tests-NetCore45")]
+//[assembly: InternalsVisibleTo("Octokit.Tests-Portable")]
